d61b71b9c3605d24bec2932365d68f197a8819db
Ivan Gamez <thecubeg@gmail.com>
2025-05-30 21:09:21 -0600

    routes

--- ArtStore/src/Components/NavBar.jsx
+++ ArtStore/src/Components/NavBar.jsx
@@ -6,9 +6,9 @@ function NavBar() {
     <>
       <header className=" flex-1/2   h-aurto py-5 px-10  ">
         <div className="  text-rose-600 text-6xl font-bold   ">
-          <a className=" block">Catalogo</a>
-          <a className=" block text-center text-9xl upper">Art Store</a>
-          <a className=" block text-right">Novedades</a>
+          <a className=" block">Artistic styles</a>
+          <a className=" block text-center text-9xl upper">The Art Store</a>
+          <a className=" block text-right">News</a>
         </div>
         <CartWidget />
         <h2 className=" text-right text-white">Created by Ivan Gamez</h2>
